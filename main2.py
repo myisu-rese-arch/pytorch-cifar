dff0e1834d3f8c594b7de8341ae2dab7aa134fc0
azibit <abdulazeezidris28@gmail.com>
2022-02-12 16:05:03 -0600

    Add base and updated code

--- main2.py
+++ main2.py
@@ -4,31 +4,16 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
+
 import torchvision
 import torchvision.transforms as transforms
 
-import os, sys
+import os, sys, time
 import argparse
 
 from models import *
 from utils import progress_bar
 
-import torch as ch
-import time
-from typing import List
-from ffcv.fields import IntField, RGBImageField
-from ffcv.fields.decoders import IntDecoder, SimpleRGBImageDecoder
-from ffcv.loader import Loader, OrderOption
-from ffcv.pipeline.operation import Operation
-from ffcv.transforms import RandomHorizontalFlip, Cutout, RandomTranslate, Convert, ToDevice, ToTensor, ToTorchImage
-from ffcv.transforms.common import Squeeze
-from ffcv.writer import DatasetWriter
-
-from tqdm import tqdm
-from  torch.cuda.amp import autocast
-from ffcv.pipeline.operation import Operation
-from ffcv.pipeline.allocation_query import AllocationQuery
-from dataclasses import replace
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -36,35 +21,6 @@ parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True)
-
-datasets = {
-	'train': trainset,
-        'test': testset
-}
-
-for (name, ds) in datasets.items():
-    writer = DatasetWriter(f'./data/{name}.beton', {
-        'image': RGBImageField(),
-        'label': IntField()
-    })
-    writer.from_indexed_dataset(ds)
-
-# Note that statistics are wrt to uin8 range, [0,255].
-CIFAR_MEAN = [125.307, 122.961, 113.8575]
-CIFAR_STD = [51.5865, 50.847, 51.255]
-
-BATCH_SIZE = 512
-
-loaders = {}
 
 # Training
 def train(epoch):
@@ -73,14 +29,11 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
-
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
-        with autocast():
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
 
@@ -102,9 +55,8 @@ def test(epoch):
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            with autocast():
-                outputs = net(inputs)
-                loss = criterion(outputs, targets)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
 
             test_loss += loss.item()
             _, predicted = outputs.max(1)
@@ -128,49 +80,45 @@ def test(epoch):
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
 
-for name in ['train', 'test']:
-    label_pipeline: List[Operation] = [IntDecoder(), ToTensor(), ToDevice('cuda:0'), Squeeze()]
-    image_pipeline: List[Operation] = [SimpleRGBImageDecoder()]
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-    # Add image transforms and normalization
-    if name == 'train':
-        image_pipeline.extend([
-            RandomHorizontalFlip(),
-            RandomTranslate(padding=2),
-            #Cutout(8, tuple(map(int, CIFAR_MEAN))), # Note Cutout is done before normalization.
-        ])
-    image_pipeline.extend([
-        ToTensor(),
-        ToDevice('cuda:0', non_blocking=True),
-        ToTorchImage(),
-        Convert(ch.float16),
-        torchvision.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
-    ])
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
-    # Create loaders
-    loaders[name] = Loader(f'./data/{name}.beton',
-                            batch_size=BATCH_SIZE,
-                            num_workers=2,
-                            order=OrderOption.RANDOM,
-                            drop_last=(name == 'train'),
-                            pipelines={'image': image_pipeline,
-                                       'label': label_pipeline})
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
 
-trainloader = loaders['train']
-testloader = loaders['test']
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
 orig_stdout = sys.stdout
-f = open('main.txt', 'w')
+f = open('main2.txt', 'w')
 sys.stdout = f
 for i in range(10):
     # Model
     print('==> Building model..')
     net = ResNet18()
-    net = net.to(memory_format=torch.channels_last).cuda()
-
+    net = net.to(device)
     if device == 'cuda':
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
@@ -194,7 +142,6 @@ for i in range(10):
         train(epoch)
         test(epoch)
         scheduler.step()
-
     print("Training finished at: ", (time.time() - starttime))
 sys.stdout = orig_stdout
 f.close()
