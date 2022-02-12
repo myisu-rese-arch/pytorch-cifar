d24af3e4937d2fd36ed803438fb7a4cbe610c537
azibit <abdulazeezidris28@gmail.com>
2022-02-12 14:04:16 -0600

    Update basic

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-
 import torchvision
 import torchvision.transforms as transforms
 
@@ -14,6 +13,19 @@ import argparse
 from models import *
 from utils import progress_bar
 
+import torch as ch
+import time
+from typing import List
+from ffcv.fields import IntField, RGBImageField
+from ffcv.fields.decoders import IntDecoder, SimpleRGBImageDecoder
+from ffcv.loader import Loader, OrderOption
+from ffcv.pipeline.operation import Operation
+from ffcv.transforms import RandomHorizontalFlip, Cutout, RandomTranslate, Convert, ToDevice, ToTensor, ToTorchImage
+from ffcv.transforms.common import Squeeze
+from ffcv.writer import DatasetWriter
+
+from tqdm import tqdm
+from  torch.cuda.amp import autocast
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -26,36 +38,83 @@ best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+# print('==> Preparing data..')
+# transform_train = transforms.Compose([
+#    transforms.RandomCrop(32, padding=4),
+#    transforms.RandomHorizontalFlip(),
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+# ])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+# transform_test = transforms.Compose([
+#    transforms.ToTensor(),
+#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+# ])
 
 trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    root='./data', train=True, download=True)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    root='./data', train=False, download=True)
 
+datasets = {
+	'train': trainset,
+        'test': testset
+}
+
+for (name, ds) in datasets.items():
+    writer = DatasetWriter(f'./data/{name}.beton', {
+        'image': RGBImageField(),
+        'label': IntField()
+    })
+    writer.from_indexed_dataset(ds)
+
+# Note that statistics are wrt to uin8 range, [0,255].
+CIFAR_MEAN = [125.307, 122.961, 113.8575]
+CIFAR_STD = [51.5865, 50.847, 51.255]
+
+BATCH_SIZE = 512
+
+loaders = {}
+
+for name in ['train', 'test']:
+    label_pipeline: List[Operation] = [IntDecoder(), ToTensor(), ToDevice('cuda:0'), Squeeze()]
+    image_pipeline: List[Operation] = [SimpleRGBImageDecoder()]
+
+    # Add image transforms and normalization
+    if name == 'train':
+        image_pipeline.extend([
+            RandomHorizontalFlip(),
+            RandomTranslate(padding=2),
+            #Cutout(8, tuple(map(int, CIFAR_MEAN))), # Note Cutout is done before normalization.
+        ])
+    image_pipeline.extend([
+        ToTensor(),
+        ToDevice('cuda:0', non_blocking=True),
+        ToTorchImage(),
+        Convert(ch.float16),
+        torchvision.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+
+    # Create loaders
+    loaders[name] = Loader(f'./data/{name}.beton',
+                            batch_size=BATCH_SIZE,
+                            num_workers=2,
+                            order=OrderOption.RANDOM,
+                            drop_last=(name == 'train'),
+                            pipelines={'image': image_pipeline,
+                                       'label': label_pipeline})
+
+
+trainloader = loaders['train']
+testloader = loaders['test']
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-# net = ResNet18()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -68,8 +127,10 @@ print('==> Building model..')
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-net = SimpleDLA()
-net = net.to(device)
+# net = SimpleDLA()
+#net = net.to(device)
+net = net.to(memory_format=torch.channels_last).cuda()
+
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
@@ -96,11 +157,14 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
+
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
+        with autocast():
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
         loss.backward()
         optimizer.step()
 
@@ -122,8 +186,9 @@ def test(epoch):
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+            with autocast():
+                outputs = net(inputs)
+                loss = criterion(outputs, targets)
 
             test_loss += loss.item()
             _, predicted = outputs.max(1)
@@ -147,8 +212,10 @@ def test(epoch):
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
 
-
+starttime = time.time()
 for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
     test(epoch)
     scheduler.step()
+
+print("Training finished at: ", (time.time() - starttime))
